eb78936369cd957d26256dd116057d2a4a7af1f8
Mohammad <momosweid164@gmail.com>
2024-02-24 08:42:19 -0600

    Finished implementing Scores and highscores. Also added background colors for when you win and lose. Currently working on implementing the Again button where the game should reset to original.

--- starter/script.js
+++ starter/script.js
@@ -4,18 +4,62 @@ document.querySelector('.message').textContent = 'Penis';
 
 console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
+document.querySelector('.secretNumber').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+//document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.again').addEventListener('click', function () {
+  let secretNumber = Math.trunc(Math.random() * 20) + 1;
+  let score = 20;
+  let highScore = 0;
+  document.querySelector('.body').style.backgroundColor = '#222';
+});
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+    //When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.highscore').textContent = score;
+    //When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.number').textContent = secretNumber;
+
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+    }
+    //When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+    }
   }
 });
