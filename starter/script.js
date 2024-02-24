d7718278baa0aa9a27318990b15829a7247c1cb1
Mohammad <momosweid164@gmail.com>
2024-02-24 07:38:32 -0600

    Check

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+/*
+document.querySelector('.message').textContent = 'Penis';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
